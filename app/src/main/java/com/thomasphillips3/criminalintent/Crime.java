59865c62f8cb04a904415c58b009120e78213d7a
Thomas Phillips <thomasphillips3@gmail.com>
2016-11-30 23:26:50 -0500

    Added getters and setters.

--- app/src/main/java/com/thomasphillips3/criminalintent/Crime.java
+++ app/src/main/java/com/thomasphillips3/criminalintent/Crime.java
@@ -13,4 +13,17 @@ public class Crime {
     public Crime() {
         mId = UUID.randomUUID();
     }
+
+    public UUID getId() {
+
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
 }
