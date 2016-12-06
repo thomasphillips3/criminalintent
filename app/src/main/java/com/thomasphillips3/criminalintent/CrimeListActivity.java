5a26c7002e3cd3d30ae97dfeef8996dc52cd0d90
Thomas Phillips <thomasphillips3@gmail.com>
2016-12-06 17:01:11 -0500

    Created new activity and fragment for displaying list of crimes.

---
+++ app/src/main/java/com/thomasphillips3/criminalintent/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.thomasphillips3.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by thomas on 12/6/16.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new Fragment();
+    }
+}
---
+++ app/src/main/java/com/thomasphillips3/criminalintent/CrimeListFragment.java
@@ -0,0 +1,11 @@
+package com.thomasphillips3.criminalintent;
+
+import android.app.Fragment;
+
+/**
+ * Created by thomas on 12/6/16.
+ */
+
+public class CrimeListFragment extends Fragment {
+
+}
