5d784f0b897d411931125d470ca780fc332177c4
Thomas Phillips <thomasphillips3@gmail.com>
2016-12-06 15:57:55 -0500

    * Added CrimeLab to create and store crimes. * Added button with crime date and time, and checkbox for isSolved. *

---
+++ app/src/main/java/com/thomasphillips3/criminalintent/CrimeLab.java
@@ -0,0 +1,45 @@
+package com.thomasphillips3.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by thomas on 12/5/16.
+ */
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context) {
+        if (null == sCrimeLab) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 4 == 0);
+            mCrimes.add(crime);
+        }
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime (UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
