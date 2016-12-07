75dfe75258a00b2560ab6a89c2b12124ea616805
Thomas Phillips <thomasphillips3@gmail.com>
2016-12-06 22:06:35 -0500

    Created new activity and fragment for displaying list of crimes.

--- app/src/main/java/com/thomasphillips3/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/thomasphillips3/criminalintent/CrimeListFragment.java
@@ -1,11 +1,31 @@
 package com.thomasphillips3.criminalintent;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by thomas on 12/6/16.
  */
 
 public class CrimeListFragment extends Fragment {
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = (RecyclerView) view
+                .findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(
+                new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 
 }
