a0ba5a06ec1e04ba5515ca511dd8690855054634
Thomas Phillips <thomasphillips3@gmail.com>
2016-12-08 23:19:14 -0800

    Added listener to respond to tapping on a crime. Throwing up a lil toast for now.

--- app/src/main/java/com/thomasphillips3/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/thomasphillips3/criminalintent/CrimeListFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -36,7 +37,7 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView mTitleTextView;
         private TextView mDateTextView;
         private CheckBox mSolvedCheckBox;
@@ -44,6 +45,7 @@ public class CrimeListFragment extends Fragment {
 
         public CrimeHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
 
             mTitleTextView = (TextView) itemView
                     .findViewById(R.id.list_item_crime_title_text_view);
@@ -51,6 +53,14 @@ public class CrimeListFragment extends Fragment {
                     .findViewById(R.id.list_item_crime_date_text_view);
             mSolvedCheckBox = (CheckBox) itemView
                     .findViewById(R.id.list_item_crime_solved_check_box);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(),
+                    mCrime.getTitle() + " clicked.",
+                    Toast.LENGTH_SHORT)
+                    .show();
         }
 
         public void bindCrime(Crime crime) {
