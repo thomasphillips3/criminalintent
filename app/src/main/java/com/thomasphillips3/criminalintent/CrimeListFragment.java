776da4a5c1088b46eec084c66075a520a0853286
Thomas Phillips <thomasphillips3@gmail.com>
2016-12-30 13:57:20 -0800

    Implemented ToolBar with New Crime button. Doesn't do anything yet though.

--- app/src/main/java/com/thomasphillips3/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/thomasphillips3/criminalintent/CrimeListFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -115,5 +117,17 @@ public class CrimeListFragment extends Fragment {
             mAdapter.notifyDataSetChanged();
             mAdapter.notifyItemChanged(mCurrentPosition);
         }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 }
