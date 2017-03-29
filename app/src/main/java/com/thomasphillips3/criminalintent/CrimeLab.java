a4cc7f41b0efff69ee9269dcc7a63be328fe64fb
Thomas Phillips <thomasphillips3@gmail.com>
2017-03-29 16:04:36 -0700

    Implemented SQLite for persistent storage.

--- app/src/main/java/com/thomasphillips3/criminalintent/CrimeLab.java
+++ app/src/main/java/com/thomasphillips3/criminalintent/CrimeLab.java
@@ -1,6 +1,13 @@
 package com.thomasphillips3.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.thomasphillips3.criminalintent.database.CrimeBaseHelper;
+import com.thomasphillips3.criminalintent.database.CrimeCursorWrapper;
+import com.thomasphillips3.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +19,8 @@ import java.util.UUID;
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (null == sCrimeLab) {
@@ -22,27 +30,84 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        List<Crime> crimes = new ArrayList<>();
+
+        CrimeCursorWrapper cursor = queryCrimes(null, null);
+
+        try {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                crimes.add(cursor.getCrime());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+        return crimes;
     }
 
     public Crime getCrime (UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
+        CrimeCursorWrapper cursor = queryCrimes(
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] { id.toString() }
+        );
+
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
             }
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
         }
-        return null;
     }
 
     public void addCrime(Crime c) {
-        mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeTable.NAME, null, values);
+
     }
 
     public void deleteCrime(Crime c) {
-        mCrimes.remove(c);
+        ContentValues values = getContentValues(c);
+
+    }
+
+    public void updateCrime (Crime crime) {
+        ContentValues values = getContentValues(crime);
+        String uuidString = crime.getId().toString();
+
+        mDatabase.update(CrimeTable.NAME, values,
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] { uuidString });
+    }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+
+        return values;
+    }
+
+    private CrimeCursorWrapper queryCrimes(String whereClause, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
+                CrimeTable.NAME,
+                null,
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+        return new CrimeCursorWrapper(cursor);
     }
 }
---
+++ app/src/main/java/com/thomasphillips3/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.thomasphillips3.criminalintent.database;
+
+/**
+ * Created by thomas on 3/19/17.
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
